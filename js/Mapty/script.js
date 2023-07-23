4cd72beb30b433b6e60f720a4dbccd5237578c4d
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-07-23 15:33:51 +0545

    pushed

--- js/Mapty/script.js
+++ js/Mapty/script.js
@@ -11,10 +11,30 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-navigator.geolocation?.getCurrentPosition(function (position) {
-  // console.log(position.coords);
-  const { longitude } = position.coords;
-  const { latitude } = position.coords;
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // console.log(position.coords);
+      const { longitude } = position.coords;
+      const { latitude } = position.coords;
 
-  console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-});
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const map = L.map("map").setView([`${latitude}`, `${longitude}`], 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([`${latitude}`, `${longitude}`])
+        .addTo(map)
+        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+        .openPopup();
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
+}
+7;
