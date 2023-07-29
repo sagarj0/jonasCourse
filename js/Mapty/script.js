86d1151f1403ff759830eb9843c8f4caeed5c5d0
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-07-29 20:06:54 +0545

    Async start

--- js/Mapty/script.js
+++ js/Mapty/script.js
@@ -23,19 +23,7 @@ class Workout {
 
   _setDescription() {
     // prettier-ignore
-    const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December",
     ];
 
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
