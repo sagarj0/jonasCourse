d92effb5acb78e50ffe718482b4a87a12b52f0f8
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-07-31 18:11:40 +0545

    pushed

--- js/Async await/script.js
+++ js/Async await/script.js
@@ -4,3 +4,7 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request = new XMLHttpRequest();
+
+request.open('GET', '');
