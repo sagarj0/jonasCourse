1c1de3e4e31d32f2bc2fce20fee14064f779273d
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-08-10 21:13:49 +0545

    pushed

--- js/Async await/script.js
+++ js/Async await/script.js
@@ -1,18 +1,54 @@
 'use strict';
 
 const btn = document.querySelector('.btn-country');
+
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest();
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
 
-request.open('GET', 'https://restcountries.com/v3.1/name/nepal');
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 
-request.send();
+  request.send();
 
-console.log(request.responseText);
+  console.log(request.responseText);
 
-request.addEventListener('load', function () {
-  console.log(this.responseText);
-});
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // console.log([Object.values(data.currencies)]);
+    const html = `
+ <article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} millon people</p>
+    <p class="country__row"><span>🗣️</span>${
+      Object.values(data.languages)[0]
+    }</p>
+    <p class="country__row"><span>💰</span>${
+      Object.values(data.currencies)[0].name
+    }</p>
+  </div>
+ </article>
+ `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('nepal');
+
+getCountryData('bangladesh');
+
+getCountryData('bharat');
+
+getCountryData('bangladesh');
