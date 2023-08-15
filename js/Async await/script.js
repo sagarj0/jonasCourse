95bdd5dcdaa7f602114508a40bf26600c71998f3
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-08-15 21:42:51 +0545

    pushed

--- js/Async await/script.js
+++ js/Async await/script.js
@@ -4,6 +4,35 @@ const btn = document.querySelector('.btn-country');
 
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function () {
+  countriesContainer.innerAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+ <article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)} millon people</p>
+    <p class="country__row"><span>🗣️</span>${
+      Object.values(data.languages)[0]
+    }</p>
+    <p class="country__row"><>💰</ span>${
+      Object.values(data.currencies)[0].name
+    }</p>
+  </div>
+ </article>
+ `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+
+  // countriesContainer.style.opacity = 1;
+};
 ///////////////////////////////////////
 /*
 const getCountryData = function (country) {
@@ -54,70 +83,146 @@ getCountryData('bharat');
 getCountryData('bangladesh');
 */
 
-const renderCountry = function (data, className = '') {
-  const html = `
- <article class="country ${className}">
-  <img class="country__img" src="${data.flags.png}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name.common}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(1)} millon people</p>
-    <p class="country__row"><span>🗣️</span>${
-      Object.values(data.languages)[0]
-    }</p>
-    <p class="country__row"><span>💰</span>${
-      Object.values(data.currencies)[0].name
-    }</p>
-  </div>
- </article>
- `;
+// const getCountryAndNeibhourData = function (country) {
+//   //ajax call country 1
+//   const request = new XMLHttpRequest();
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 
-  countriesContainer.style.opacity = 1;
-};
+//   request.send();
 
-const getCountryAndNeibhourData = function (country) {
-  //ajax call country 1
-  const request = new XMLHttpRequest();
+//   console.log(request.responseText);
 
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     //render country 1
+//     renderCountry(data);
+//     // console.log([Object.values(data.currencies)]);
 
-  request.send();
+//     //get neighbour country
 
-  console.log(request.responseText);
+//     const [neighbour] = data.borders;
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    //render country 1
-    renderCountry(data);
-    // console.log([Object.values(data.currencies)]);
+//     if (!neighbour) return;
 
-    //get neighbour country
+//     //ajax call country 2
 
-    const [neighbour] = data.borders;
+//     const request2 = new XMLHttpRequest();
 
-    if (!neighbour) return;
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
 
-    //ajax call country 2
+//     request2.send();
 
-    const request2 = new XMLHttpRequest();
+//     request2.addEventListener('load', function () {
+//       // console.log(this.responseText);
+//       const [data2] = JSON.parse(this.responseText);
+//       renderCountry(data2, ' neighbour');
+//     });
+//   });
+// };
 
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+// getCountryAndNeibhourData('nepal');
 
-    request2.send();
+// // getCountryAndNeibhourData('bharat');
+// const request = fetch(`https://restcountries.com/v3.1/name/nepal`);
 
-    request2.addEventListener('load', function () {
-      // console.log(this.responseText);
-      const [data2] = JSON.parse(this.responseText);
-      renderCountry(data2, ' neighbour');
+// console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+/*
+const getCountryData = function (country) {
+  //country 1
+
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+
+      //country 2
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'))
+    .catch(err => {
+      console.error(`${err} error`);
+      renderError(`Something went wrong ${err.message}. try again`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
     });
-  });
 };
 
-getCountryAndNeibhourData('nepal');
+btn.addEventListener('click', function () {
+  getCountryData('nepal');
+});
 
-// getCountryAndNeibhourData('bharat');
+getCountryData('abcdef');
+*/
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+
+GOOD LUCK 😀
+*/
+
+// console.log(navigator);
+// console.log(Math);
+
+// let longitude=0, latitude=0;
+
+const makeRequest = function ([lat, long]) {
+  fetch(`https://geocode.xyz/${lat},${long}?geoit=json`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+  setTimeout(makeRequest, 1000);
+};
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+
+      makeRequest([latitude, longitude]);
+      // console.log(latitude, longitude);
+      // makeRequest();
+    },
+    function () {
+      console.log('error');
+    }
+  );
+}
+
+const whereAmI = function () {};
