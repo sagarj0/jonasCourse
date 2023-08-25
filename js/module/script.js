1bf934c8bd43e5edbc7d82b6985deb5c5655fcde
Sagar Joshi <89570837+sagarj0@users.noreply.github.com>
2023-08-25 05:19:19 +0000

    pushed

---
+++ js/module/script.js
@@ -0,0 +1,7 @@
+// console.log("sagar");
+//Importing module
+import { addToCart } from "./shopingCart.js";
+
+console.log("imported from module");
+
+addToCart("bread", 5);
---
+++ js/module/shopingCart.js
@@ -0,0 +1,12 @@
+//Exporting module
+console.log("exporting from module");
+
+const shippingCost = 83;
+
+const cart = [];
+
+export const addToCart = function (produnct, quantity) {
+  cart.push({ produnct, quantity });
+
+  console.log(`${produnct} ${quantity}  added to cart`);
+};
