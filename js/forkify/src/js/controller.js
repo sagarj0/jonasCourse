29853167cc4e8a32ea93aa9a0451a37e86eee6ec
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-08-28 17:24:11 +0545

    Pushed

--- js/forkify/src/js/controller.js
+++ js/forkify/src/js/controller.js
@@ -11,3 +11,19 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipie = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/768b6fb3-58cf-4c0f-8430-c15e04bb1d7a'
+    );
+
+    const data = await res.json();
+
+    console.log(res, data);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipie();
