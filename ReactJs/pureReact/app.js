1c20f46f579c13aa6a9bb00af74c5b0de0f315f9
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-09-05 12:27:38 +0545

    pushed

--- ReactJs/pureReact/app.js
+++ ReactJs/pureReact/app.js
@@ -1,1 +1,3 @@
-console.log(fetch("https://jsonplaceholder.typicode.com/todos/1"));
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
