d3ae684625b0fac2c821949204c65dfc838ffa3f
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-10-13 09:54:27 +0545

    Server error

--- nodejs/node-farm/index.js
+++ nodejs/node-farm/index.js
@@ -1,2 +1,43 @@
-const hello = "hello";
-console.log(hello);
+const fs = require("fs");
+
+const http = require("http");
+
+//////////////////////
+/////FILES
+// const { text } = require("node:stream/consumers");
+
+// const hello = "hello world";
+// console.log(hello);
+
+// console.log(fs);
+
+// const textIn = fs.readFileSync("nodejs/node-farm/txt/input.txt", "utf-8");
+
+// console.log(textIn);
+
+// console.log("Current working directory:", process.cwd());
+
+// const textOut = `hello there guys i am currently learning nodejs `;
+
+// fs.writeFileSync("nodejs/node-farm/txt/input.txt", textOut + textIn);
+
+// console.log("written");
+// helo guys i am currently learn]  ing nodejs
+
+// non blocking asynchronous way
+
+// fs.readFile("nodejs/node-farm/txt/start.txt", "utf-8", (err, data) => {
+// console.log(data);
+// });
+
+// console.log("will read file");
+
+//////////////////////////
+///SERVER
+const server = http.createServer((req, res) => {
+  res.end("hello from the server");
+});
+
+server.listen(8000, "127.0.0.1 ", () => {
+  console.log("listening to the port 8000");
+});
