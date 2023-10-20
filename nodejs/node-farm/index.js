6ad80ec4fb2752697fabd1e3639d901b6101412e
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-10-20 08:02:45 +0545

    Learned about HTML templating

--- nodejs/node-farm/index.js
+++ nodejs/node-farm/index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 
 const http = require("http");
 
+const url = require("url");
+
 //////////////////////
 /////FILES
 // const { text } = require("node:stream/consumers");
@@ -34,10 +36,28 @@ const http = require("http");
 
 //////////////////////////
 ///SERVER
+
+const data = fs.readFileSync("./dev-data/data.json", "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  res.end("hello from the server");
+  const pathName = req.url;
+  console.log(pathName);
+
+  if (pathName === "/overview" || pathName === "/") {
+    res.end("This is overview");
+  } else if (pathName === "/product") {
+    res.end("This is product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("<h1 style = color:red >Page not found</h1>");
+  }
+  // res.end("hello from the server");
 });
 
-server.listen(8000, "127.0.0.1 ", () => {
+server.listen(8000, () => {
   console.log("listening to the port 8000");
 });
