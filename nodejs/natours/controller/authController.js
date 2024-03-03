aad0a5d72dd26b327e5a1eb21452efe722f5b135
sagar-joshi83 <joshhisagar9375@gmail.com>
2024-03-03 23:29:48 +0545

    jwt and password hashed

--- nodejs/natours/controller/authController.js
+++ nodejs/natours/controller/authController.js
@@ -3,6 +3,12 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -12,11 +18,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign(
-    { id: newUser._id },
-    process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES_IN }
-  );
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -48,4 +50,12 @@ exports.login = catchAsync(async (req, res, next) => {
       new AppError('Incorrect email or password', 401)
     );
   }
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    message: 'User logged in successfully',
+    token,
+  });
 });
